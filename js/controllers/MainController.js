7992eadef39e6d20ccfeac3f8b740f1af6bf3293
seikomapua <46939532+seikomapua@users.noreply.github.com>
2019-01-29 00:28:08 +0800

    Add files via upload

---
+++ js/controllers/MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope) {
+
+$scope.name = 'Kenzo, Kazumi, Seiko and Anjo';
+
+$scope.title = 'Web Developer';
+
+$scope.description = 'Hello my name is Rai Kenzo Mapua AKA "Combo Panda". I am 4 years old. I can read, sing and dance.'
+}])
